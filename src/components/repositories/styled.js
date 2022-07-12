b3d2a2059fe875df64dcabb3b7ec20f73be16b29
Patrick Marques <patrickmarques2009@hotmail.com>
2022-07-12 16:51:19 -0300

    Estilização inicial dos botões Repositories e Starred

--- src/components/repositories/styled.js
+++ src/components/repositories/styled.js
@@ -1,10 +1,53 @@
 import styled from "styled-components";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 
-export const WrapperTabs = styled(Tabs)``;
+export const WrapperTabs = styled(Tabs)`
 
-export const WrapperTabList = styled(TabList)``;
+    font-size: 16px;
+    width: 50%;
+    margin-top: 16px;
 
-export const WrapperTab = styled(Tab)``;
+`;
 
-export const WrapperTabPanel = styled(TabPanel)``;
+export const WrapperTabList = styled(TabList)`
+
+    list-style-type: none;
+    padding: 4px;
+    display: flex;
+    margin: 0;
+
+`;
+WrapperTabList.tabsRole = 'TabList';
+
+export const WrapperTab = styled(Tab)`
+
+    border-radius: 16px;
+    border: 1px solid #ccc;
+    padding: 8px;
+    user-select: none;
+    cursor: pointer;
+    margin: 4px;
+
+    &:focus{
+        outline: none;
+    }
+
+    &.is-selected {
+        border-top-left-radius: 16px;
+        border-top-right-radius: 16px;
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        border-bottom: 1px solid transparent;
+    }
+
+`;
+WrapperTabList.tabsRole = 'Tab';
+
+export const WrapperTabPanel = styled(TabPanel)`
+
+    paddding: 8px;
+    border: 1px solid '#ccc';
+    box-shadow: 0 0 1em rgba(0, 0, 0, 0.5);
+
+`;
+WrapperTabList.tabsRole = 'TabPanel';
