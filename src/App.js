f55b58eb8455cbb4a81c4041737d5a4f114bcfc0
Patrick Marques <patrickmarques2009@hotmail.com>
2022-07-12 16:20:45 -0300

    Criado o componente Repositories com a lib React Tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "./components/layout";
 import Profile from "./components/profile";
+import Repositories from "./components/repositories";
 import { ResetCSS } from "./global/resetCSS"
 
 // link para o JSON do user https://api.github.com/users/PkMs7
@@ -9,13 +10,10 @@ function App() {
   return (
   <main>
     <ResetCSS />
-    <Layout>
-        <div>
+      <Layout>
           <Profile />
-        </div>
-      <div>Repositories</div>
-      <div>Starreds</div>
-    </Layout>
+          <Repositories />
+      </Layout>
   </main>
   );
 }
